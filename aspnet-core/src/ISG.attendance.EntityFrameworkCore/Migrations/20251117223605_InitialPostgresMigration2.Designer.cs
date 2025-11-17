914d41a9e34c9b3db79f045fdd0afc8a71baef06
mo Hady <mohamed.abdelhadi@saudisoft.com>
2025-11-18 00:38:11 +0200

    fix FE and BE running version

--- aspnet-core/src/ISG.attendance.EntityFrameworkCore/Migrations/20251117223605_InitialPostgresMigration2.Designer.cs
+++ aspnet-core/src/ISG.attendance.EntityFrameworkCore/Migrations/20251117223605_InitialPostgresMigration2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ISG.attendance.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace ISG.attendance.Migrations
 {
     [DbContext(typeof(attendanceDbContext))]
-    partial class attendanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251117223605_InitialPostgresMigration2")]
+    partial class InitialPostgresMigration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

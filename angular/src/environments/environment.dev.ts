ab0483c08ce114966d1b2a59e2788b8183888daa
mo Hady <mohamed.abdelhadi@saudisoft.com>
2025-11-20 18:59:47 +0200

    dev setting and env file !!

---
+++ angular/src/environments/environment.dev.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'attendance.fe.demo.egisg.com';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'attendance',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://attendance.demo.egisg.com/',
+    redirectUri: baseUrl,
+    clientId: 'attendance_App',
+    responseType: 'code',
+    scope: 'offline_access attendance',
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://attendance.demo.egisg.com',
+      rootNamespace: 'ISG.attendance',
+    },
+  },
+} as Environment;
